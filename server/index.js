6bc83ae81155845122fe639b9e2ade202f8e6a27
Thibault FONTAINE <thibaultfontaine303@gmail.com>
2023-04-04 15:31:19 +0200

    update node

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ import express from 'express';
 const app = express();
 
 
-app.listen(PORT, () => console.log(`Le serveur est lancer sur le port ${PORT}`))
+app.listen(3001, () => console.log(`Le serveur est lancer sur le port ${3001}`))
 
