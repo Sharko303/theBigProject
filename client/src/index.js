56ff5956820ded47507592918724bbaac8d3897a
Thibault FONTAINE <thibaultfontaine303@gmail.com>
2023-06-09 17:07:05 +0200

    info

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
+// on appelle la function de get /me et apres on compare notre id avec ceux récupérer dans les events
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
